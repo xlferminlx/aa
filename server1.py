7dbd3e663fa8a3597d7f5a0656d8ad9149e8c6ed
xlferminlx <termanpoo@gmail.com>
2025-04-26 11:02:26 -0500

    Update server1.py

--- server1.py
+++ server1.py
@@ -1,35 +1,45 @@
 from socket import *
 from threading import Thread
 
-clients = {}  # Stores {client_socket: address}
+clients = {}  # stores {client_socket: address}
 PORT = 12345
 
 def handle_client(client_socket, address):
-    clients[client_socket] = address
-    welcome_msg = f"Welcome {address[0]}! Type 'list' to see users or 'msg <ip> <message>' to message someone."
+    username = client_socket.recv(1024).decode().strip()
+    
+    # if i don't get it to work remove these lines, current problem if the same username gets inputed it will enter an infinite loop
+    if any(existing_username == username for sock, (addr, existing_username) in clients.items()):
+        client_socket.send("Username already taken, good bye".encode())
+        client_socket.close()
+        return
+    # this is fine
+    
+    clients[client_socket] = (address, username)
+    welcome_msg = f"Welcome {username}! Type 'list' to see users or 'msg <username> <message>' to message someone."
     client_socket.send(welcome_msg.encode())
 
     try:
         while True:
             msg = client_socket.recv(1024).decode()
             if msg.lower() == "list":
-                client_list = "\n".join([f"{addr[0]}" for sock, addr in clients.items()])
+                client_list = "\n".join([f"{username} ({addr[0]})" for sock, (addr, username) in clients.items()])
                 client_socket.send(f"Connected clients:\n{client_list}".encode())
 
             elif msg.startswith("msg"):
                 parts = msg.split(" ", 2)
                 if len(parts) == 3:
-                    target_ip, message = parts[1], parts[2]
+                    target_username = parts[1]
+                    message = parts[2]
                     sent = False
-                    for sock, addr in clients.items():
-                        if addr[0] == target_ip and sock != client_socket:
-                            sock.send(f"From {address[0]}: {message}".encode())
+                    for sock, (addr, username) in clients.items():
+                        if username == target_username and sock != client_socket:
+                            sock.send(f"From {clients[client_socket][1]}: {message}".encode())
                             sent = True
                             break
                     if not sent:
-                        client_socket.send("Client not found.".encode())
+                        client_socket.send("User not found.".encode())
                 else:
-                    client_socket.send("Invalid message format. Use: msg <ip> <message>".encode())
+                    client_socket.send("Invalid message format. Use: msg <username> <message>".encode())
 
             elif msg.lower() == "close":
                 break
@@ -38,7 +48,7 @@ def handle_client(client_socket, address):
     except:
         pass
     finally:
-        print(f"{address} disconnected")
+        print(f"{clients[client_socket][1]} disconnected")
         clients.pop(client_socket, None)
         client_socket.close()
 
